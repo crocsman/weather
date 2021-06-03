eaf6d384b4566acd80831afed899107fb5cbc613
adam pi <adamphillips238@gmail.com>
2021-06-03 16:57:07 -0700

    DHT code, using example

--- DHT_Sensor.cpp
+++ DHT_Sensor.cpp
@@ -1,3 +1,80 @@
+nclude <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define MAXTIMINGS	85
+#define DHTPIN		7
+int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+ 
+void read_dht11_dat()
+{
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j		= 0, i;
+	float	f; 
+ 
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+ 
+	pinMode( DHTPIN, OUTPUT );
+	digitalWrite( DHTPIN, LOW );
+	delay( 18 );
+	digitalWrite( DHTPIN, HIGH );
+	delayMicroseconds( 40 );
+	pinMode( DHTPIN, INPUT );
+ 
+	for ( i = 0; i < MAXTIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHTPIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHTPIN );
+ 
+		if ( counter == 255 )
+			break;
+ 
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			dht11_dat[j / 8] <<= 1;
+			if ( counter > 16 )
+				dht11_dat[j / 8] |= 1;
+			j++;
+		}
+	}
+ 
+	if ( (j >= 40) &&
+	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+	{
+		f = dht11_dat[2] * 9. / 5. + 32;
+		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
+	}else  {
+		printf( "Data not good, skip\n" );
+	}
+}
+ 
+int main( void )
+{
+	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+ 
+	if ( wiringPiSetup() == -1 )
+		exit( 1 );
+ 
+	while ( 1 )
+	{
+		read_dht11_dat();
+		delay( 1000 ); 
+	}
+ 
+	return(0);
+/*
+
 #include <wiringPi.h> //includes delay and microdelay func
 #include <iostream>
 //#include <stdlib.h> //needed for delay func
@@ -9,7 +86,7 @@ void start()
 {
 	//setup input vars
 	//use 8bit type to fit 8bit input	
-	uint8_t
+	//uint8_t
 
 
 	//setup pin GPIO16 as output
@@ -49,3 +126,6 @@ int main()
 //sources
 /*
 ://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/   */
+
+
+*/
